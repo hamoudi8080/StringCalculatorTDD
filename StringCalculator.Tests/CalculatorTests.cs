2c84a66452f09d70f1b391a7ae2693069e957df2
Mohammed Ali Resho <mar@redhorse.dk>
2023-03-15 15:11:15 +0100

    simple cases with Test Driven Develepment

---
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,103 @@
+﻿using FluentAssertions;
+
+namespace StringCalculator.Tests;
+
+public class CalculatorTests
+{
+    [Theory]
+    [InlineData("",0)]
+    [InlineData("1",1)]
+    [InlineData("1,3",4)]
+    public void Add_AddUpToTwoNumbers_WhenStringIsValid(string calculation, int expected)
+    {
+        //arrange
+        var sut = new Calculator();
+
+
+        //act
+        var result = sut.Add(calculation);
+
+
+        //assert
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [InlineData("1,2,3",6)]
+    [InlineData("10,90,10,20",130)]
+    
+    public void Add_AddUpToAnyNumber_WhenStringIsValid(string calculation, int expected)
+    {
+        //arrange
+        var sut = new Calculator();
+
+
+        //act
+        var result = sut.AddUpToAnyNumber_(calculation);
+
+
+        //assert
+        result.Should().Be(expected);
+    }
+    
+    
+    
+    [Theory]
+    [InlineData("1,\n2,3",6)]
+    [InlineData("10,\n90,10,\n20",130)]
+    
+    public void Add_AddsNumbersUsingNewLineDelimiter_WhenStringIsValid(string calculation, int expected)
+    {
+        //arrange
+        var sut = new Calculator();
+
+
+        //act
+        var result = sut.AddsNumbersUsingNewLineDelimiter(calculation);
+
+
+        //assert
+        result.Should().Be(expected);
+    }
+    
+    
+    
+    [Theory]
+    [InlineData("//;\n1;2",3)]
+    [InlineData("//;\n1;2;4",7)]
+    
+    public void Add_AddsNumbersUsingCustomDelimiter_WhenStringIsValid(string calculation, int expected)
+    {
+        //arrange
+        var sut = new Calculator();
+
+
+        //act
+        var result = sut.AddsNumbersUsingCustomDelimiter(calculation);
+
+
+        //assert
+        result.Should().Be(expected);
+    }
+    
+    
+        
+    
+    [Theory]
+    [InlineData("1,2,-1","-1")]
+    [InlineData("//;\n1;-2;-4","-2,-4")]
+    
+    public void Add_ShouldThrowAnException_WhenNegitiveNumbersAreUsed(string calculation, string negativeNumbers)
+    {
+        //arrange
+        var sut = new Calculator();
+
+
+        //act
+        Action action = () => sut.Add_ShouldThrowAnException(calculation);
+
+
+        //assert
+        action.Should().Throw<Exception>().WithMessage("Negatives are not allowed" + negativeNumbers);
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,105 @@
+﻿using System.Globalization;
+
+namespace StringCalculator;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        var splitNumbers = numbers.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (!splitNumbers.Any())
+        {
+            return 0;
+        }
+
+        if (splitNumbers.Length == 1)
+        {
+            return int.Parse(splitNumbers[0]);
+        }
+
+        int theInt = int.Parse(splitNumbers[0]) + int.Parse(splitNumbers[1]);
+        return theInt;
+    }
+
+
+    public int AddUpToAnyNumber_(string numbers)
+    {
+        //StringSplitOptions.RemoveEmptyEntries, which tells the method to remove any empty substrings from the resulting array.
+
+
+        //he Select method is then used to convert each element of the resulting array of substrings from a string to an integer,
+        //using the int.Parse method.
+        //This produces an IEnumerable<int> sequence of integers.
+        var splitNumbers = numbers.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+
+
+        return splitNumbers.Sum();
+    }
+
+
+    public int AddsNumbersUsingNewLineDelimiter(string numbers)
+    {
+        var delimiters = new[] { ',', '\n' };
+        var splitNumbers = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+
+
+        return splitNumbers.Sum();
+    }
+
+
+    public int AddsNumbersUsingCustomDelimiter(string numbers)
+    {
+        var delimiters = new List<char> { ',', '\n' };
+
+        if (numbers.StartsWith("//"))
+        {
+            var splitOnFirstNewLine = numbers.Split(new[] { '\n' }, 2);
+            var customDelimiter = splitOnFirstNewLine[0].Replace("//", string.Empty).Single();
+            delimiters.Add(customDelimiter);
+            numbers = splitOnFirstNewLine[1];
+        }
+
+        var splitNumbers = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+
+
+        return splitNumbers.Sum();
+    }
+
+
+    public int Add_ShouldThrowAnException(string numbers)
+    {
+        var delimiters = new List<char> { ',', '\n' };
+
+        if (numbers.StartsWith("//"))
+        {
+            var splitOnFirstNewLine = numbers.Split(new[] { '\n' }, 2);
+            var customDelimiter = splitOnFirstNewLine[0].Replace("//", string.Empty).Single();
+            delimiters.Add(customDelimiter);
+            numbers = splitOnFirstNewLine[1];
+        }
+
+        var splitNumbers = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse);
+
+        var negativeNumbers = splitNumbers.Where(x => x < 0).ToList();
+        
+        
+        foreach (var potentiallyNegativeNumber in splitNumbers)
+        {
+            if (potentiallyNegativeNumber < 0)
+            {
+                negativeNumbers.Add(potentiallyNegativeNumber);                
+            }
+        }
+
+        if (negativeNumbers.Any())
+        {
+            throw new Exception("Negative numbers are not allowed: " + string.Join(",", negativeNumbers));
+        }
+        return splitNumbers.Sum();
+    }
+}
